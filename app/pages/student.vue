de7b4c98c878fcef15edc322d61b6140240a200d
Cham Roeun <tunchamroeun@cloudware.com.kh>
2026-01-30 10:10:36 +0700

    add feature/student add data grid

--- app/pages/student.vue
+++ app/pages/student.vue
@@ -1,5 +1,20 @@
 <template>
  <div>
   This is student page
+  <div>
+    Data Grid
+    <table>
+        <thead>
+        <tr>
+            <th>Header</th>
+        </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Record</td>
+            </tr>
+        </tbody>
+    </table>
+  </div>
  </div> 
 </template>
