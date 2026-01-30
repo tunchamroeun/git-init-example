e4d57092112a8bcacf71ab73353c89040df28308
Cham Roeun <tunchamroeun@cloudware.com.kh>
2026-01-30 14:47:36 +0700

    improve index, add table and toast

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,20 +1,47 @@
+<script setup lang="ts">
+const toast = useToast()
+
+function showToast() {
+  const eventDate = new Date(Date.now() + Math.random() * 31536000000)
+  const formattedDate = eventDate.toLocaleDateString('en-US', {
+    month: 'short',
+    day: 'numeric',
+    year: 'numeric'
+  })
+
+  toast.add({
+    title: 'Event added to calendar',
+    description: `This event is scheduled for ${formattedDate}.`,
+    icon: 'i-lucide-calendar-days'
+  })
+}
+</script>
+
 <template>
-  <div class="flex p-8">
-    <div class="flex w-6xl gap-2">
+  <ul class="flex flex-col w-6xl gap-2 p-8">
+    <li>
       <UButton
         to="/samnang"
       >
         Samnang
       </UButton>
+    </li>
+    <li>
       <UButton to="/ravy">
         Ravy
       </UButton>
-
+    </li>
+    <li>
       <UButton
         to="/chamroeun"
       >
         Cham Roeun
       </UButton>
-    </div>
-  </div>
+    </li>
+    <li>
+      <UButton @click="showToast()">
+        Show toast
+      </UButton>
+    </li>
+  </ul>
 </template>
