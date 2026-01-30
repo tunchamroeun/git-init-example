36821287087f89af4fc528721665387cdd2fc973
Ravy <motravy123456789@gmail.com>
2026-01-30 16:06:45 +0700

    edit fucntion file index

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 const toast = useToast()
 
-function showToast() {
+function showToastRavy() {
   const eventDate = new Date(Date.now() + Math.random() * 31536000000)
   const formattedDate = eventDate.toLocaleDateString('en-US', {
     month: 'short',
@@ -42,7 +42,7 @@ function showToast() {
       </UButton>
     </li>
     <li>
-      <UButton @click="showToast()">
+      <UButton @click="showToastRavy()">
         Show toast
       </UButton>
     </li>
